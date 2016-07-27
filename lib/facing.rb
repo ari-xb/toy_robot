07c6ebad3dbd486db2ea62280b61e01c571c443d
Kasun Maldeni <kasun>
2016-07-27 10:04:59 +0000

    Implement facing and turning functionality

--- lib/facing.rb
+++ lib/facing.rb
@@ -1,7 +1,27 @@
 class Facing
+  attr_accessor :direction
+  
+  FACING = [:north, :east, :south, :west]
+  
   def initialize(facing)
+    self.direction = facing
+  end
+  
+  def left_turn
+    turn(-1)
+  end
+  
+  def right_turn
+    turn(1)
   end
   
   def is_valid?
+    FACING.include?(self.direction)
+  end
+  
+  private
+  def turn(direction)
+    facing_index = FACING.index(self.direction)
+    self.direction = FACING.rotate(direction)[facing_index]
   end
 end
