aaee8e9c81a76c86e27627b3d63ad051fd481fe8
Kasun Maldeni <kasun>
2016-07-27 03:28:20 +0000

    Specs for robot when trying to execute a valid 'place' command

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'robot'
 
 describe Robot do
   subject(:robot) { Robot.new }
@@ -13,20 +14,34 @@ describe Robot do
     end
     
     it 'should not allow the robot to move' do
-      expect { robot.execute("MOVE") }.to_not change { robot.coordinates }
+      expect { robot.execute('MOVE') }.to_not change { robot.coordinates }
     end
     
     it 'should not allow the robot to turn left' do
-      expect { robot.execute("LEFT") }.to_not change { robot.facing }
+      expect { robot.execute('LEFT') }.to_not change { robot.facing }
     end
     
     it 'should not allow the robot to turn right' do
-      expect { robot.execute("RIGHT") }.to_not change { robot.facing }
+      expect { robot.execute('RIGHT') }.to_not change { robot.facing }
     end
     
     it 'should not allow the robot to report its position' do
       expect($stdout).to_not receive(:write)
-      robot.execute("REPORT")
+      robot.execute('REPORT')
+    end
+  end
+  
+  context "when trying to execute a valid 'place' command" do
+    it 'should not accept coordinates outside the range of the grid' do
+      expect { robot.execute('PLACE 1,6,NORTH') }.to_not change { robot.coordinates }
+    end
+    
+    it 'should not accept coordinates that are not integers' do
+      expect { robot.execute('PLACE a,2,NORTH') }.to_not change { robot.coordinates }
+    end
+    
+    it 'should not accept a direction outside of north, east, south, west' do
+      expect { robot.execute('PLACE 1,1,RANDOM') }.to_not change { robot.coordinates }
     end
   end
 end
