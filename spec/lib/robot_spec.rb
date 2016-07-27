87acf09280af1a94a0185fef1bfc011b9fab55cd
Kasun Maldeni <kasun>
2016-07-27 04:19:59 +0000

    Specs for robot turning logic

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -32,7 +32,7 @@ describe Robot do
   end
   
   context "when trying to execute a valid 'place' command" do
-    it 'should not accept coordinates outside the range of the grid' do
+    it 'should not be placed when the coordinates are outside the range of the grid' do
       expect { robot.execute('PLACE 1,6,NORTH') }.to_not change { robot.coordinates }
     end
     
@@ -42,6 +42,54 @@ describe Robot do
     
     it 'should not accept a direction outside of north, east, south, west' do
       expect { robot.execute('PLACE 1,1,RANDOM') }.to_not change { robot.coordinates }
+    end
+    
+    it 'should be placed on the grid when the coordinates and direction are valid' do
+      expect { robot.execute('PLACE 1,2,NORTH') }.to change { robot.coordinates }
+    end
+  end
+  
+  context "after a valid 'place' command is executed" do
+    # Left Turn
+    it 'should change its facing direction from north to west when turning left' do
+      place = "PLACE 1,2,NORTH"
+      expect { robot.execute("#{place} LEFT") }.to change { robot.facing }.from('north').to('west')
+    end
+    
+    it 'should change its facing direction from west to south when turning left' do
+      place = "PLACE 1,2,WEST"
+      expect { robot.execute("#{place} LEFT") }.to change { robot.facing }.from('west').to('south')
+    end
+    
+    it 'should change its facing direction from south to east when turning left' do
+      place = "PLACE 1,2,SOUTH"
+      expect { robot.execute("#{place} LEFT") }.to change { robot.facing }.from('south').to('east')
+    end
+    
+    it 'should change its facing direction from east to north when turning left' do
+      place = "PLACE 1,2,EAST"
+      expect { robot.execute("#{place} LEFT") }.to change { robot.facing }.from('east').to('north')
+    end
+    
+    # Right Turn
+    it 'should change its facing direction from north to east when turning right' do
+      place = "PLACE 1,2,NORTH"
+      expect { robot.execute("#{place} RIGHT") }.to change { robot.facing }.from('north').to('east')
+    end
+    
+    it 'should change its facing direction from east to south when turning right' do
+      place = "PLACE 1,2,EAST"
+      expect { robot.execute("#{place} RIGHT") }.to change { robot.facing }.from('east').to('south')
+    end
+    
+    it 'should change its facing direction from south to west when turning right' do
+      place = "PLACE 1,2,SOUTH"
+      expect { robot.execute("#{place} RIGHT") }.to change { robot.facing }.from('south').to('west')
+    end
+    
+    it 'should change its facing direction from west to north when turning right' do
+      place = "PLACE 1,2,WEST"
+      expect { robot.execute("#{place} RIGHT") }.to change { robot.facing }.from('west').to('north')
     end
   end
 end
